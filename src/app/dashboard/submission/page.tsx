6a17d772c8057013d2bb5568cc0245810f993254
Gauri <gauri100405@gmail.com>
2025-09-23 09:39:53 +0000

    data is not submitting nor going to the cmlre approval queue section

--- src/app/dashboard/submission/page.tsx
+++ src/app/dashboard/submission/page.tsx
@@ -1,7 +1,6 @@
-
 "use client";
 
-import React, { useState, useTransition } from "react";
+import React, { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
