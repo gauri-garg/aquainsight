10f988a84c8729e80ffa13fef971cc931085e0ef
Gauri <gauri100405@gmail.com>
2025-09-27 13:26:01 +0000

    replce this icon to other fish icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   title: "AquaInsight",
   description: "Unifying oceanographic, fisheries, and molecular biology datasets.",
   icons: {
-    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐟</text></svg>',
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐠</text></svg>',
   },
 };
 
